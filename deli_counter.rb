5ed9d8371337bfea4566e88580d8d0e7d9a0b636
Jackson Ramos <jackramos021495@gmail.com>
2020-05-05 21:01:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+ if array.length > 0
+   line_number = 1
+   array.each do |line|
+     puts "The line is currently: #{line_number}. #{line}"
+     line_number +=1
+   end
+ else
+   puts "The line is currently empty"
+ end
+end
+
+def take_a_number
+
+end
+
+def now_serving
+
+end
